4d49e1f5c92eaf7be3ccec51534d46d4243048da
saptaparnaMaity <75085120+saptaparnaMaity@users.noreply.github.com>
2024-06-27 10:40:06 +0530

    sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,22 +1,18 @@
 void setup()
 {
-  //  initialize the serial communication
+ 
   Serial.begin(9600);
 }
 
 void loop()
 {
   
-  //  read the state of pin 2
-  //  convert it to string format
   
   String state = String(digitalRead(2));
-  
-  //  send that data towards arduino 2
+   
   
   Serial.print(state);
   
-  //  wait for 500 ms
   
   delay(500);
 }
